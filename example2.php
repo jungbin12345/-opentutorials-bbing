a8aab8370049d4ca313521783f8457600795941a
bbing <jungbin12345@naver.com>
2013-04-24 08:19:35 -0700

    index.html

---
+++ example2.php
@@ -0,0 +1,8 @@
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html;charset=utf-8"	</head>
+<body>
+	<?php
+	echo 'hello? ' . $_REQUEST['nickname'] .  '. your job is ' . $_REQUEST['job'].  '!!!!'; ?>
+</body>
+</html>
